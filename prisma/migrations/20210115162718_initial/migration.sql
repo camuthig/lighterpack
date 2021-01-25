18448af6b13b9fc44e460a41b7344f48a9ee53f4
Chris Muthig <camuthig@gmail.com>
2021-01-24 19:09:16 -0800

    Get working implementation of saveLibrary and returnLibrary
    This implementation allows for using the frontend of the application
    without any alterations. This is not the optimal way to design the
    database or the APIs but allows for the possibility of A/B testing the
    system with existing data pretty easily.
    
    This is not tested for performance.

--- prisma/migrations/20210115162718_initial/migration.sql
+++ prisma/migrations/20210115162718_initial/migration.sql
@@ -9,16 +9,18 @@ CREATE TABLE users (
 
 CREATE TABLE app_configurations (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL UNIQUE,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER UNIQUE,
     total_unit VARCHAR(10) NOT NULL,
     item_unit VARCHAR(10) NOT NULL,
     last_list INTEGER, -- default list
-    library_version INTEGER NOT NULL DEFAULT 0, -- sequence
+    id_sequence INTEGER NOT NULL DEFAULT 0, -- sequence
     show_sidebar BOOLEAN NOT NULL DEFAULT TRUE,
     show_images BOOLEAN NOT NULL DEFAULT FALSE,
     show_prices BOOLEAN NOT NULL DEFAULT FALSE,
-    show_worn BOOLEAN NOT NULL DEFAULT FALSE,
-    show_consumable BOOLEAN NOT NULL DEFAULT FALSE,
+    show_worn BOOLEAN NOT NULL DEFAULT TRUE,
+    show_consumable BOOLEAN NOT NULL DEFAULT TRUE,
     show_list_description BOOLEAN NOT NULL DEFAULT FALSE,
     currency_symbol CHAR(1),
     CONSTRAINT fk__app_configurations__user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
@@ -26,59 +28,114 @@ CREATE TABLE app_configurations (
 
 CREATE TABLE gear_lists (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    library_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
     external_id VARCHAR(255) UNIQUE,
     name VARCHAR(255) NOT NULL,
     description TEXT,
-    -- TODO Add other fields for weights and what not.
+    total_weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_worn_weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_consumable_weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_base_weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_pack_weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_price NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_consumable_price NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    total_qty INTEGER NOT NULL DEFAULT 0,
+    UNIQUE (user_id, library_id),
     CONSTRAINT fk__gear_lists__user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE gear_list_positions (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    gear_list_id INTEGER NOT NULL,
-    UNIQUE (user_id, gear_list_id),
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    gear_list_library_id INTEGER,
+    position INTEGER,
+    UNIQUE (user_id, gear_list_library_id),
     CONSTRAINT fk__gear_list_positions__user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    CONSTRAINT fk__gear_list_positions__gear_list_id FOREIGN KEY (gear_list_id) REFERENCES gear_lists(id) ON DELETE CASCADE
+    CONSTRAINT fk__gear_list_positions__gear_list FOREIGN KEY (user_id, gear_list_library_id) REFERENCES gear_lists(user_id, library_id) ON DELETE CASCADE
 );
 
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
-    gear_list_id INTEGER NOT NULL,
+    library_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    gear_list_library_id INTEGER,
     name VARCHAR(255) NOT NULL,
-    CONSTRAINT fk__categories__gear_list_id FOREIGN KEY (gear_list_id) REFERENCES gear_lists(id) ON DELETE CASCADE
+    subtotal_weight  NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    subtotal_worn_weight  NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    subtotal_consumable_weight  NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    subtotal_price  NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    subtotal_consumable_price  NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
+    subtotal_qty INTEGER NOT NULL DEFAULT 0,
+    UNIQUE (user_id, library_id),
+    CONSTRAINT fk__categories__gear_list FOREIGN KEY (user_id, gear_list_library_id) REFERENCES gear_lists(user_id, library_id) ON DELETE CASCADE
 );
 
 CREATE TABLE category_positions (
     id SERIAL PRIMARY KEY,
-    gear_list_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    -- Add other fields for weight and what not.
-    UNIQUE (gear_list_id, category_id),
-    CONSTRAINT fk__category_positions__gear_list_id FOREIGN KEY (gear_list_id) REFERENCES gear_lists(id) ON DELETE CASCADE,
-    CONSTRAINT fk__category_positions__category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    gear_list_library_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    category_library_id INTEGER,
+    position INTEGER,
+    UNIQUE (user_id, gear_list_library_id, category_library_id),
+    CONSTRAINT fk__category_positions__gear_list FOREIGN KEY (user_id, gear_list_library_id) REFERENCES gear_lists(user_id, library_id) ON DELETE CASCADE,
+    CONSTRAINT fk__category_positions__category FOREIGN KEY (user_id, category_library_id) REFERENCES categories(user_id, library_id) ON DELETE CASCADE
 );
 
 CREATE TABLE items (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    library_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     weight NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
     weight_unit VARCHAR(10) NOT NULL,
-    price TEXT, -- TODO Figure out the expected definition for the price field
+    price NUMERIC(14, 2) NOT NULL DEFAULT 0.00,
     image_url TEXT,
     url TEXT,
+    UNIQUE (user_id, library_id),
     CONSTRAINT fk__items__user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE item_positions (
     id SERIAL PRIMARY KEY,
-    category_id INTEGER NOT NULL,
-    item_id INTEGER NOT NULL,
-    UNIQUE (category_id, item_id),
-    CONSTRAINT fk__item_positions__category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
-    CONSTRAINT fk__item_positions__item_id FOREIGN KEY (item_id) REFERENCES items(id) ON DELETE CASCADE
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    user_id INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    category_library_id INTEGER,
+    item_library_id INTEGER,
+    position INTEGER,
+    -- We have to allow null to allow for cascade delete to work with prisma
+    -- See: https://github.com/prisma/prisma/issues/2057
+    qty INTEGER NOT NULL DEFAULT 1,
+    is_worn BOOLEAN NOT NULL DEFAULT FALSE,
+    is_consumable BOOLEAN NOT NULL DEFAULT FALSE,
+    star INTEGER NOT NULL DEFAULT 0,
+    UNIQUE (user_id, category_library_id, item_library_id),
+    CONSTRAINT fk__item_positions__category FOREIGN KEY (user_id, category_library_id) REFERENCES categories(user_id, library_id) ON DELETE CASCADE,
+    CONSTRAINT fk__item_positions__item FOREIGN KEY (user_id, item_library_id) REFERENCES items(user_id, library_id) ON DELETE CASCADE
 );
-
--- server/v2/endpoints.js
+++ server/v2/endpoints.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
 const path = require('path');
@@ -83,16 +84,46 @@ router.post('/register', async (req, res) => {
 
                 awesomeLog(req, 'Creating new user.');
 
-                await prisma.user.create({
-                    data: {
-                        username,
-                        passwordHash: hash,
-                        email,
-                        token,
-                        syncToken: 0,
-                        // TODO Add app configuration creation here
-                    }
-                  })
+                try {
+                    await prisma.user.create({
+                        data: {
+                            username,
+                            passwordHash: hash,
+                            email,
+                            token,
+                            syncToken: 0,
+                            appConfiguration: {
+                                create: {
+                                    totalUnit: 'oz',
+                                    itemUnit: 'oz',
+                                    currencySymbol: '$',
+                                    showSidebar: true,
+                                    showWorn: true,
+                                    showConsumable: true,
+                                    showImages: false,
+                                    showPrices: false,
+                                    showListDescription: false,
+                                    idSequence: 1,
+                                    lastList: 1,
+                                },
+                            },
+                            gearLists: {
+                                create: {
+                                    libraryId: 1,
+                                    name: "Your First List",
+                                    gearListPosition: {
+                                        create: {
+                                            position: 0,
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    });
+                } catch(err) {
+                    return res.status(500).json();
+                }
+
 
                 const out = { username, syncToken: 0 };
                 res.cookie('lp', token, { path: '/', maxAge: 365 * 24 * 60 * 1000 });
@@ -107,20 +138,144 @@ router.post('/signin', (req, res) => {
 });
 
 
-function returnLibrary(req, res, user) {
+async function returnLibrary(req, res, user) {
     awesomeLog(req, user.username);
     if (!user.syncToken) {
         user.syncToken = 0;
-        db.users.save(user);
+        await prisma.user.update({
+            where: { id: user.id },
+            data: { syncToken: 0 },
+        });
     }
-    return res.json({ username: user.username, library: JSON.stringify(user.library), syncToken: user.syncToken });
+
+    let library = await prisma.user.findUnique({
+        where: { id: user.id },
+        include: {
+            appConfiguration: true,
+            gearLists: {
+                include: {
+                    categoryPositions: {
+                        orderBy: { position: 'asc' }
+                    },
+                    categories: {
+                        include: {
+                            items: {
+                                orderBy: { position: 'asc' },
+                                include: {
+                                    item: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            items: true,
+            gearListPositions: {
+                orderBy: { position: 'asc' },
+            },
+        },
+    });
+
+    const orderList = function (orderingRelations, relationKey, toBeOrdered, primaryKey) {
+        let listOrdering = {}
+        orderingRelations.forEach((pos, index) => {
+            listOrdering[pos[relationKey]] = index;
+        });
+
+        let orderedList = [];
+        toBeOrdered.forEach(elem => {
+            orderedList[listOrdering[elem[primaryKey]]] = elem;
+        });
+
+        return orderedList;
+    }
+
+    let out = {
+        version: '0.3',
+        idMap: {},
+        items: library.items.map((item) => { return {
+            id: item.libraryId,
+            name: item.name,
+            description: item.description,
+            weight: item.weight,
+            authorUnit: item.weightUnit,
+            price: item.price,
+            image: '',
+            imageUrl: item.imageUrl,
+            url: item.url,
+        }}),
+        categories: [],
+        lists: [],
+        sequence: library.appConfiguration.idSequence,
+        defaultListId: library.appConfiguration.lastList,
+        totalUnit: library.appConfiguration.totalUnit,
+        itemUnit: library.appConfiguration.itemUnit,
+        showSidebar: library.appConfiguration.showSidebar,
+        showImages: library.appConfiguration.showImages,
+        optionalFields: {
+            images: library.appConfiguration.showImages,
+            price: library.appConfiguration.showPrices,
+            worn: library.appConfiguration.showWorn,
+            consumable: library.appConfiguration.showConsumable,
+            listDescription: library.appConfiguration.showListDescription,
+        },
+        currencySymbol: library.appConfiguration.currencySymbol,
+    }
+    // order the lists
+    let categories = []
+    let gearLists = orderList(library.gearListPositions, 'gearListLibraryId', library.gearLists, 'libraryId')
+    gearLists = gearLists.map(gearList => {
+        categories = categories.concat(gearList.categories.map(category => {
+            return {
+                id: category.libraryId,
+                name: category.name,
+                subtotalWeight: category.subtotalWeight,
+                subtotalWornWeight: category.subtotalWornWeight,
+                subtotalConsumableWeight: category.subtotalConsumableWeight,
+                subtotalPrice: category.subtotalPrice,
+                subtotalConsumablePrice: category.subtotalConsumablePrice,
+                subtotalQty: category.subtotalQty,
+                categoryItems: category.items.map(itemPos => {
+                    return {
+                        itemId: itemPos.itemLibraryId,
+                        qty: itemPos.qty,
+                        worn: itemPos.isWorn,
+                        consumable: itemPos.isConsumable,
+                        star: itemPos.star,
+                    };
+                }),
+
+            };
+        }));
+
+        return {
+            id: gearList.libraryId,
+            name: gearList.name,
+            description: gearList.description,
+            externalId: gearList.externalId,
+            totalWeight: gearList.totalWeight,
+            totalWornWeight: gearList.totalWornWeight,
+            totalConsumableWeight: gearList.totalConsumableWeight,
+            totalBaseWeight: gearList.totalBaseWeight,
+            totalPackWeight: gearList.totalPackWeight,
+            totalPrice: gearList.totalPrice,
+            totalConsumablePrice: gearList.totalConsumablePrice,
+            totalQty: gearList.totalQty,
+            categoryIds: gearList.categoryPositions.map(pos => pos.categoryLibraryId),
+        };
+    });
+
+    out.lists = gearLists;
+    out.categories = categories;
+
+    return res.json({ username: user.username, library: JSON.stringify(out), syncToken: user.syncToken });
 }
 
 router.post('/saveLibrary', (req, res) => {
     authenticateUser(req, res, saveLibrary);
 });
 
-function saveLibrary(req, res, user) {
+async function saveLibrary(req, res, user) {
     if (typeof req.body.syncToken === 'undefined') {
         return res.status(400).send("Please refresh this page to upgrade to the latest version of LighterPack.");
     }
@@ -143,13 +298,209 @@ function saveLibrary(req, res, user) {
         return res.status(400).json({ errors: [{ message: 'An error occurred while saving your data - unable to parse library. If this persists, please contact support.' }] });
     }
 
-    user.library = library;
-    user.syncToken++;
-    db.users.save(user, () => {
-        awesomeLog(req, user.username);
-
-        return res.status(200).json({ message: 'success', syncToken: user.syncToken });
+    let configUpdate = prisma.user.update({
+        where: { id: user.id },
+        data: {
+            syncToken: {
+                increment: 1,
+            },
+            appConfiguration: {
+                update: {
+                    idSequence: library.sequence,
+                    lastList: library.defaultListId,
+                    totalUnit: library.totalUnit,
+                    itemUnit: library.itemUnit,
+                    showSidebar: library.showSidebar,
+                    showImages: library.showImages,
+                    showImages: library.optionalFields.images,
+                    showPrices: library.optionalFields.price,
+                    showWorn: library.optionalFields.worn,
+                    showConsumable: library.optionalFields.consumable,
+                    showListDescription: library.optionalFields.listDescription,
+                    currencySymbol: library.currencySymbol,
+                },
+            },
+        },
     });
+
+    // Delete existing gear lists, categories, items, etc.
+    let gearListPosDelete = prisma.gearListPosition.deleteMany({
+        where: { userId: user.id }
+    });
+    let categoryPosDelete = prisma.categoryPosition.deleteMany({
+        where: { gearList: { userId: user.id } },
+    });
+    let itemPosDelete = prisma.itemPosition.deleteMany({
+        where: { item: { userId: user.id } },
+    });
+    let itemDelete = prisma.item.deleteMany({
+        where: { userId: user.id },
+    });
+    let categoryDelete = prisma.item.deleteMany({
+        where: { userId: user.id },
+    });
+    let gearListDelete = prisma.gearList.deleteMany({
+        where: { userId: user.id },
+    });
+
+    let gearListInserts = [];
+    let categoryInserts = [];
+    let itemInserts = [];
+    let itemPosInserts = [];
+
+    let categoryIndex = 0;
+    let gearListIndex = 0;
+    let itemIndex = 0;
+
+    // add everything back in
+    itemInserts = library.items.map(itemObj => {
+        return prisma.item.create({
+            data: {
+                libraryId: itemObj.id,
+                name: itemObj.name,
+                description: itemObj.description,
+                weight: itemObj.weight,
+                weightUnit: itemObj.authorUnit,
+                price: itemObj.price,
+                imageUrl: itemObj.imageUrl,
+                url: itemObj.url,
+                user: {
+                    connect: {
+                        id: user.id,
+                    },
+                },
+            },
+        });
+    });
+
+    let categoryObjsById = {}
+    library.categories.forEach(categoryObj => {
+        categoryObjsById[categoryObj.id] = categoryObj;
+    });
+
+    gearListInserts = library.lists.map(gearList => {
+        return prisma.gearList.create({
+            data: {
+                libraryId: gearList.id,
+                name: gearList.name,
+                description: gearList.description,
+                externalId: gearList.externalId || null,
+                totalWeight: gearList.totalWeight,
+                totalWornWeight: gearList.totalWornWeight,
+                totalConsumableWeight: gearList.totalConsumableWeight,
+                totalBaseWeight: gearList.totalBaseWeight,
+                totalPackWeight: gearList.totalPackWeight,
+                totalPrice: gearList.totalPrice,
+                totalConsumablePrice: gearList.totalConsumablePrice,
+                totalQty: gearList.totalQty,
+                user: {
+                    connect: {
+                        id: user.id,
+                    },
+                },
+                gearListPosition: {
+                    create: {
+                        position: gearListIndex++,
+                        user: {
+                            connect: {
+                                id: user.id,
+                            }
+                        },
+                    },
+                },
+                categories: {
+                    // WIP The ordering of these categories within the list are always getting reversed
+                    create: gearList.categoryIds.map(categoryId => {
+                        let categoryObj = categoryObjsById[categoryId];
+                        return {
+                            libraryId: categoryObj.id,
+                            name: categoryObj.name,
+                            subtotalWeight: categoryObj.subtotalWeight,
+                            subtotalWornWeight: categoryObj.subtotalWornWeight,
+                            subtotalConsumableWeight: categoryObj.subtotalConsumableWeight,
+                            subtotalPrice: categoryObj.subtotalPrice,
+                            subtotalConsumablePrice: categoryObj.subtotalConsumablePrice,
+                            subtotalQty: categoryObj.subtotalQty,
+                            user: {
+                                connect: {
+                                    id: user.id,
+                                },
+                            },
+                            categoryPosition: {
+                                create: {
+                                    position: categoryIndex++,
+                                    user: {
+                                        connect: {
+                                            id: user.id,
+                                        },
+                                    },
+                                    gearList: {
+                                        connect: {
+                                            userId_libraryId: {
+                                                userId: user.id,
+                                                libraryId: gearList.id,
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        };
+                    }),
+                },
+            },
+        });
+    });
+
+    library.categories.map(categoryObj => {
+        categoryObj.categoryItems.forEach(itemObj => {
+            itemPosInserts.push(prisma.itemPosition.create({
+                data: {
+                    position: itemIndex++,
+                    category: {
+                        connect: {
+                            userId_libraryId: {
+                                userId: user.id,
+                                libraryId: categoryObj.id,
+                            }
+                        },
+                    },
+                    item: {
+                        connect: {
+                            userId_libraryId: {
+                                userId: user.id,
+                                libraryId: itemObj.itemId,
+                            }
+                        }
+                    },
+                    isWorn: Boolean(itemObj.worn),
+                    isConsumable: Boolean(itemObj.consumable),
+                    qty: itemObj.qty,
+                    star: itemObj.star,
+                },
+            }));
+        })
+    });
+
+    let waitFor = [
+        configUpdate,
+        gearListPosDelete,
+        categoryPosDelete,
+        itemPosDelete,
+        itemDelete,
+        categoryDelete,
+        gearListDelete,
+    ];
+    waitFor = waitFor.concat(
+        itemInserts,
+        gearListInserts,
+        itemPosInserts,
+    );
+
+    await prisma.$transaction(waitFor);
+
+    awesomeLog(req, user.username);
+
+    return res.status(200).json({ message: 'success', syncToken: ++user.syncToken });
 }
 
 router.post('/externalId', (req, res) => {
@@ -356,7 +707,11 @@ function deleteAccount(req, res, user) {
                 return Promise.reject(new Error('An error occurred, please try logging out and in again.'));
             }
 
-            await prisma.user.delete({ where: { id: user.id } })
+            try {
+                await prisma.user.delete({ where: { id: user.id } })
+            } catch (err) {
+                return res.status(500).json({ message: 'An error occurred, please try again later.'});
+            }
 
             return res.status(200).json({ message: 'success' });
         })
